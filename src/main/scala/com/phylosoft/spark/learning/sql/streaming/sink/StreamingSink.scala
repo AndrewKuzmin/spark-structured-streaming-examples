49ac8b45c8d1c7e52f29f218d0672e19229b1a41
Andrew Kuzmin <andrew.kuzmin@gmail.com>
2019-03-16 20:30:33 +0200

    Memory sink was added.

--- src/main/scala/com/phylosoft/spark/learning/sql/streaming/sink/StreamingSink.scala
+++ src/main/scala/com/phylosoft/spark/learning/sql/streaming/sink/StreamingSink.scala
@@ -1,9 +1,25 @@
 package com.phylosoft.spark.learning.sql.streaming.sink
 
+import java.io.File
+
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.streaming.StreamingQuery
 
 trait StreamingSink {
+
+  val checkpointLocation: String = "file:///" + new File("checkpoint").getAbsolutePath
+
+  // Default trigger (runs micro-batch as soon as it can)
+  // Trigger.ProcessingTime(0L)
+  //
+  // ProcessingTime trigger with two-seconds micro-batch interval
+  // Trigger.ProcessingTime(2.seconds)
+  //
+  // One-time trigger
+  // Trigger.Once()
+  //
+  // Continuous trigger with one-second checkpointing interval
+  // Trigger.Continuous("1 second")
 
   def writeStream(data: DataFrame): StreamingQuery
 
